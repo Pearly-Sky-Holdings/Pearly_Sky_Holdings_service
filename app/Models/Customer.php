616b21b34b26e6ac5f6c792ed311d9c3d7f2f44a
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-02-18 21:46:19 +0530

    add content

---
+++ app/Models/Customer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+
+class Customer extends Model
+{
+    use HasFactory, HasApiTokens;
+
+    protected $primaryKey = 'customer_id';
+    public $incrementing = true;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'company',
+        'country',
+        'city',
+        'province',
+        'postal_code',
+        'contact',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
---
+++ app/Services/CustomerService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerService
+{
+    public function save(Request $request)
+    {
+        $customer = new Customer();
+        $customer->fill($request->all());
+        $customer->save();
+        return $customer;
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        // Update logic here
+        $customer = Customer::find($id);
+        if ($customer) {
+            $customer->update($request->all());
+            return $customer;
+        }
+        return null;
+    }
+}
