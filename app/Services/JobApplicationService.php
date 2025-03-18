a273621b58e48e06509ecdc538250a158bea79ec
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-03-18 13:39:33 +0530

    update

--- app/Services/JobApplicationService.php
+++ app/Services/JobApplicationService.php
@@ -2,26 +2,26 @@
 
 namespace App\Services;
 
-use App\Models\Customer;
+use App\Models\JobApplication;
 use Illuminate\Http\Request;
 
 class JobApplicationService
 {
     public function save(Request $request)
     {
-        $customer = new Customer();
-        $customer->fill($request->all());
-        return $customer->save();
+        $data = new JobApplication();
+        $data->fill($request->all());
+        return $data->save();
     }
 
 
     public function update(Request $request, $id)
     {
         // Update logic here
-        $customer = Customer::find($id);
-        if ($customer) {
-            $customer->update($request->all());
-            return $customer;
+        $data = JobApplication::find($id);
+        if ($data) {
+            $data->update($request->all());
+            return $data;
         }
         return null;
     }
