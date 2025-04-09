012bf6431a7e9b5876866fbfddf1eeb632b908ed
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-04-09 16:21:20 +0530

    mail transaction

--- app/Services/ServiceDetailsService.php
+++ app/Services/ServiceDetailsService.php
@@ -311,13 +311,13 @@ class ServiceDetailsService
             $qrCodePath = $this->generateQrCode($result['order']->order_id);
             $result['order']->update(['qr_code' => $qrCodePath]);
 
-            DB::commit();
-
             $this->sendTranslatedEmail(
                 $result['customer']->email, 
                 $result['customerId'],
                 $result['country']
             );
+
+            DB::commit();
 
             return response()->json([
                 'status' => 'success',
